9aef0c43674f7a7ca092c7a572a7638835fffd57
Catravine <caroline@carolinecourtney.com>
2023-09-09 17:44:57 -0400

    stubs for auth methods

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,8 +7,28 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make api request to sign up with that email and password
+    // if we sign up, modify state to say we're authenticated
+    // if it fails, we need to reflect an error message somewhere
+  }
+}
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // similar to sign up
+  }
+}
+
+const signout = (dispatch) => {
+  return () => {
+    //sign out
+  }
+}
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signin, signup, signout },
   { isSignedIn: false }
 );
