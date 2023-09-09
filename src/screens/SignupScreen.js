5484447759121814ae31b1e83850983cdd6e56ad
Catravine <caroline@carolinecourtney.com>
2023-09-09 17:25:56 -0400

    input fields state

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
       <Spacer>
@@ -11,9 +14,22 @@ const SignupScreen = ({ navigation }) => {
           Sign Up for Tracker
         </Text>
       </Spacer>
-      <Input label="Email" />
+      <Input 
+        label="Email" 
+        autoCapitalize='none'
+        autCorrect={false}
+        value={email} 
+        onChangeText={setEmail}
+      />
       <Spacer />
-      <Input label="Password" />
+      <Input 
+        secureTextEntry
+        label="Password" 
+        autoCapitalize='none'
+        autCorrect={false}
+        value={password} 
+        onChangeText={setPassword} 
+      />
       <Spacer>
         <Button title="Sign Up" />
       </Spacer>
@@ -31,8 +47,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    marginBottom: 200
-
+    marginBottom: '10%',
+    marginBottom: 100,
   }
 });
 
