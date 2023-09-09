c8f3c9671f039ef74c4d261c335d202c34d9ece5
Catravine <caroline@carolinecourtney.com>
2023-09-09 18:36:34 -0400

    signin success flow; also needed a clever way to get access to navigate object, from outside the screen compoents listed within the navigation object

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,26 +1,30 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import createDataContext from "./createDataContext";
 import trackerApi from '../api/tracker';
+import { navigate } from '../navigationRef';
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
       return {...state, errorMessage: action.payload }
+    case 'signup':
+      return { token: action.payload, errorMessage: '' }
     default: 
       return state;
   }
 };
 
-const signup = (dispatch) => {
-  return async ({ email, password }) => {
-    try {
-      const response = await trackerApi.post('/signup', { email, password });
-      console.log(response.data);
-    } catch (err) {
-      dispatch({ 
-        type: 'add_error', 
-        payload: 'Something went wrong with sign up' 
-      });
-    }
+const signup = (dispatch) => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post('/signup', { email, password });
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({ type: 'signup', payload: response.data.token });
+    navigate('TrackList');
+  } catch (err) {
+    dispatch({ 
+      type: 'add_error', 
+      payload: 'Something went wrong with sign up' 
+    });
   }
 }
 
@@ -39,5 +43,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signup, signout },
-  { isSignedIn: false, errorMessage: '' }
+  { token: null, errorMessage: '' }
 );
