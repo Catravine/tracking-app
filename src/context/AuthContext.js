04f5620358b9acbb99ad5de15ecd1b351c79cdab
Catravine <caroline@carolinecourtney.com>
2023-09-17 13:20:00 -0400

    implemented sign in, clear error on navigating away, resolving first thing whether we are logged in!

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,18 +7,24 @@ const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
       return {...state, errorMessage: action.payload }
-    case 'signup':
+    case 'signin':
       return { token: action.payload, errorMessage: '' }
+    case 'clear_error_message':
+      return { ...state, errorMessage: '' }
     default: 
       return state;
   }
 };
 
-const signup = (dispatch) => async ({ email, password }) => {
+const clearErrorMessage = dispatch => () => {
+  dispatch({ type: 'clear_error_message' })
+}
+
+const signup = dispatch => async ({ email, password }) => {
   try {
     const response = await trackerApi.post('/signup', { email, password });
     await AsyncStorage.setItem('token', response.data.token);
-    dispatch({ type: 'signup', payload: response.data.token });
+    dispatch({ type: 'signin', payload: response.data.token });
     navigate('TrackList');
   } catch (err) {
     dispatch({ 
@@ -28,9 +34,27 @@ const signup = (dispatch) => async ({ email, password }) => {
   }
 }
 
-const signin = (dispatch) => {
-  return ({ email, password }) => {
-    // similar to sign up
+const tryLocalSignin = dispatch => async () => {
+  const token = await AsyncStorage.getItem('token');
+  if (token) {
+    dispatch({ type: 'signin', payload: token });
+    navigate('TrackList');
+  } else {
+    navigate('LoginFlow');
+  }
+}
+
+const signin = dispatch => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post('/signin', { email, password });
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({ type: 'signin', payload: response.data.token });
+    navigate('TrackList') 
+  } catch (err) {
+    dispatch({
+      type: 'add_error',
+      payload: 'Something went wrong with sign in'
+    });
   }
 }
 
@@ -42,6 +66,6 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signup, signout },
+  { signin, signup, signout, clearErrorMessage, tryLocalSignin },
   { token: null, errorMessage: '' }
 );
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { NavigationEvents } from 'react-navigation'
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SigninScreen = () => {
+  const { state, signin, clearErrorMessage } = useContext(AuthContext);
+
   return (
     <View style={styles.container}>
+      <NavigationEvents
+        onWillBlur={clearErrorMessage}
+      />
       <AuthForm 
         headerText="Sign In to Your Account"
-        errorMessage=""
-        onSubmit={() => {}}
+        errorMessage={state.errorMessage}
+        onSubmit={signin}
         submitButtonText="Sign In"
       />
       <NavLink 
