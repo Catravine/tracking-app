672b5b295c475b7d78ca38343ecf24d2952261a1
Catravine <caroline@carolinecourtney.com>
2023-09-17 15:07:12 -0400

    showing current position as a circle only for now

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,12 +1,14 @@
 // import '../_mockLocation'; // ONLY for testing!
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation'
 import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
   const [error, setError] = useState(null)
   const startWatching = async () => {
     try {
@@ -19,7 +21,7 @@ const TrackCreateScreen = () => {
         timeInterval: 1000,
         distanceInterval: 10
       }, (location) => {
-        console.log(location);
+        addLocation(location);
       });
     } catch (e) {
       setError(e);
