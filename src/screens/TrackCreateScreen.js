a5d8762397ac137e781208c1c0806416dfb69bae
Catravine <caroline@carolinecourtney.com>
2023-09-23 16:34:28 -0400

    starting to allow a sereies of locations to make a track

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -6,16 +6,20 @@ import { SafeAreaView, withNavigationFocus } from 'react-navigation'
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
 import useLocation from '../hooks/useLocation';
+import TrackForm from '../components/TrackForm';
 
 const TrackCreateScreen = ({ isFocused }) => {
-  const { addLocation } = useContext(LocationContext);
-  cosnt [err] = useLocation(isFocused, addLocation)
+  const { state, addLocation } = useContext(LocationContext);
+  cosnt [err] = useLocation(isFocused, (location) => {
+    addLocation(location, state.recording)
+  })
   
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text h2>Create a Track</Text>
       <Map />
       {error ? <Text>Please enable location services</Text> : null}
+      <TrackForm />
     </SafeAreaView>
   )
 }
