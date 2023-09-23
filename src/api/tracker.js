f56249480714b4c7a888b70db0079ca374179f3d
Catravine <caroline@carolinecourtney.com>
2023-09-23 18:07:01 -0400

    automatically add the token if it exists in AsyncStorage, to all our api requests

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: 'https://9361-2600-1700-8c20-c5d0-153d-9f6d-66c3-c2b4.ngrok-free.app'
-})
+});
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,10 +2,12 @@ import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 const TrackForm = () => {
   const { state: { name, recording }, 
     startRecording, stopRecording, changeName } = useContext(LocationContext);
+  const [saveTrack] = useSaveTrack();
 
   return (
     <>
@@ -20,7 +22,7 @@ const TrackForm = () => {
       </Spacer>
       <Spacer>
         {!recording && LocationSubscriber.length ?
-          <Button title="Save Recording" /> : null
+          <Button title="Save Recording" onPress={saveTrack} /> : null
         }
       </Spacer>
     </>
